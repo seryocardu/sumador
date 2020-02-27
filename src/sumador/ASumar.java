9a9cd35e62c2ef88f3f8e1ae9d01ac204c13ea72
pelop <pelop@RayDark>
2020-02-27 11:56:40 +0100

    Creamos el proyecto, la clase ASumar y TestASumar para realizar las diferentes pruebas.

---
+++ src/sumador/ASumar.java
@@ -0,0 +1,13 @@
+package sumador;
+
+public class ASumar {
+
+	public ASumar(int numero) {
+	}
+
+	public String mostrar() {
+		return null;
+	}
+	
+
+}
---
+++ test/sumador/Test.java
@@ -0,0 +1,5 @@
+package sumador;
+
+public @interface Test {
+
+}
